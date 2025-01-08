0d23b50806a1cfc29d51578aa9370ac9bd000751
Ingo <me@ingowolf.au>
2025-01-08 11:59:55 +1100

    start adding elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+  import { Input } from "$lib/components/ui/input/index.js";
+</script>
+
 <div class="w-screen h-screen flex flex-col justify-center items-center">
-  aa
+  <div class="w-96 max-w-full">
+    <h1 class="text-3xl">Smasbook Calculator</h1>
+    
+    <Input placeholder="GB of RAM" type="number" />
+  </div>
 </div>
