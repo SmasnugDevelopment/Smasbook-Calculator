236964bc23b1f4587c02eb61f599838ca7b62ba0
Ingo <me@ingowolf.au>
2025-01-08 12:41:14 +1100

    mdofiy a bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,19 +24,28 @@
       <Select.Trigger>{formFactor || "Form factor"}</Select.Trigger>
       <Select.Content>
         <Select.Item value="Laptop">Laptop</Select.Item>
+        <Select.Item value="Mini PC">Mini PC</Select.Item>
         <Select.Item value="PC">PC</Select.Item>
       </Select.Content>
     </Select.Root>
-    <Input placeholder="GB of RAM" type="number" bind:value={ram} />
-    <Input
-      placeholder="Screen size in inches"
-      type="number"
-      bind:value={screensize}
-    />
-    <div class="flex items-center space-x-2">
-      <Checkbox id="touchscreen" bind:checked={touchscreen} />
-      <Label for="touchscreen">Touchscreen</Label>
-    </div>
+    <Label for="ram">RAM</Label>
+    <Input placeholder="GB of RAM" type="number" id="ram" bind:value={ram} />
+    {#if formFactor === "Laptop"}
+      <Label for="screensize">Screen size in inches</Label>
+
+      <Input
+        placeholder="Screen size in inches"
+        type="number"
+        id="screensize"
+        bind:value={screensize}
+      />
+
+      <div class="flex items-center space-x-2">
+        <Checkbox id="touchscreen" bind:checked={touchscreen} />
+        <Label for="touchscreen">Touchscreen</Label>
+      </div>
+    {/if}
+
     <h2 class="text-xl">Operating Systems</h2>
     <div class="flex items-center space-x-2">
       <Checkbox id="windows" bind:checked={windows} />
@@ -57,13 +66,13 @@
     {#if formFactor}
       <h2 class="text-xl">You have a:</h2>
       <span class="text-4xl">
-        {#if formFactor === "PC"}Smasbox{:else}Smasbook{/if}
+        {#if formFactor === "Laptop"}Smasbook{:else}Smasbox{/if}
         {#if linux == true}Pro{/if}
         {#if oscount > 1}Plus{/if}
-        {#if screensize >= 14}Max{/if}
+        {#if (screensize >= 14 && formFactor == "Laptop") || formFactor == "PC"}Max{/if}
         {#if ram >= 16}Ultra{/if}
         {#if oscount == 1 && chromeos}Lite{/if}
-        {#if touchscreen}Touch{/if}
+        {#if touchscreen && formFactor == "Laptop"}Touch{/if}
       </span>
     {/if}
   </div>
