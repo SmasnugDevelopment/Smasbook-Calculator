51a2c0f6cf787028a780b68e88436fcc6c35598c
Ingo <me@ingowolf.au>
2025-01-08 12:16:48 +1100

    add label and checkboxes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { Input } from "$lib/components/ui/input/index.js";
   import * as Select from "$lib/components/ui/select/index.js";
+  import { Checkbox } from "$lib/components/ui/checkbox/index.js";
+  import { Label } from "$lib/components/ui/label/index.js";
 
   let formFactor = $state("");
   let ram = $state(0);
@@ -17,5 +19,26 @@
       </Select.Content>
     </Select.Root>
     <Input placeholder="GB of RAM" type="number" bind:value={ram} />
+    <h2 class="text-xl">Operating Systems</h2>
+    <div class="flex items-center space-x-2">
+      <Checkbox id="windows" />
+      <Label for="windows">Windows</Label>
+    </div>
+    <div class="flex items-center space-x-2">
+      <Checkbox id="linux" />
+      <Label for="linux">Linux</Label>
+    </div>
+    <div class="flex items-center space-x-2">
+      <Checkbox id="chromeos" />
+      <Label for="chromeos">ChromeOS</Label>
+    </div>
+    <div class="flex items-center space-x-2">
+      <Checkbox id="bsd" />
+      <Label for="bsd">BSD</Label>
+    </div>
+    <div class="flex items-center space-x-2">
+      <Checkbox id="other" />
+      <Label for="other">Other</Label>
+    </div>
   </div>
 </div>
