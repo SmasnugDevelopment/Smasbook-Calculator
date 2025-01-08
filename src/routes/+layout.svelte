daf87237f3ef806269f5a7984264c688c5319d49
Ingo <me@ingowolf.au>
2025-01-08 12:57:13 +1100

    change font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import "../app.css";
+  let { children } = $props();
+
+  import "@fontsource/inter";
 </script>
 
 {@render children()}
+
+<style>
+  :global(body) {
+    font-family: "Inter", sans-serif;
+  }
+</style>
